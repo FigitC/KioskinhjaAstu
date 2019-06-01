0cb008964007946b7f63876be0eb606bc058c0f4
TheMaekir <45757703+TheMaekir@users.noreply.github.com>
2019-06-01 14:22:47 +0200

    Made a Product class. Not finished

---
+++ KioskinHjaAstu/ProductTest/ProductTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KioskinHjaAstu;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void TestToString()
+        {
+            //Arrange
+            Product unitTest = new Product(1, "0.5l Pepsi Max", 18.5, true);
+
+            //Act
+            string expectedValue = "Product: #1 - 0.5l Pepsi Max, 18,5kr.";
+            string calculatedValue = unitTest.ToString();
+
+            //Assert
+            Assert.AreEqual(expectedValue, calculatedValue);
+        }
+    }
+}
---
+++ KioskinHjaAstu/ProductTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProductTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProductTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6fcdf6e5-1a9c-4c40-9d8f-d01527ee7e5c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
