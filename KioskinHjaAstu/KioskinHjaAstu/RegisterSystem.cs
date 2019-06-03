325b4156bf70e9606f04dc5aab5a3fb6e7967ed6
TheMaekir <45757703+TheMaekir@users.noreply.github.com>
2019-06-03 15:24:04 +0200

    added class: register

---
+++ KioskinHjaAstu/KioskinHjaAstu/RegisterSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KioskinHjaAstu
+{
+    class RegisterSystem
+    {
+        //Methods
+        public void BuyProduct(User user, Product product)
+        {
+
+        }
+        AddCreditsToAccount(user, amount) { }
+        ExecuteTransaction(transaction) { }
+        GetProductByID() { }
+        
+    }
+}
