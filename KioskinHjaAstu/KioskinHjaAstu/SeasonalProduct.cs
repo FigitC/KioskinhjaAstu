59bdf37757a0f271fc4d80040bfa9e32054fb676
TheMaekir <45757703+TheMaekir@users.noreply.github.com>
2019-06-03 13:05:26 +0200

    Added Seasonal Product

---
+++ KioskinHjaAstu/KioskinHjaAstu/SeasonalProduct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KioskinHjaAstu
+{
+    public class SeasonalProduct : Product
+    {
+        //Constructor
+        public SeasonalProduct(string name, double price, bool active, bool canBeBoughtOnCredit, DateTime seasonEndDate) : base(name, price, active, canBeBoughtOnCredit)
+        {
+            seasonStartDate = DateTime.Now;
+            this.seasonEndDate = seasonEndDate;
+        }
+
+        //Class fields
+        public DateTime seasonStartDate = new DateTime();
+        public DateTime seasonEndDate = new DateTime();
+
+        // Methods
+        public override string ToString()
+        {
+            return "SeasonalProduct: #" + productID.ToString() + " - " + name.ToString() + ", " + price.ToString() + "kr." + "Expiration date: " + seasonEndDate.ToString();
+        }
+
+    }
+}
