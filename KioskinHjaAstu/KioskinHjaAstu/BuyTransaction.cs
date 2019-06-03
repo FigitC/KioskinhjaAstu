2fb16a743eda93dc2d18b09a822a08351f186ce6
janushannesarson <45720841+janushannesarson@users.noreply.github.com>
2019-06-03 15:55:48 +0200

    Added userinterface class

--- KioskinHjaAstu/KioskinHjaAstu/BuyTransaction.cs
+++ KioskinHjaAstu/KioskinHjaAstu/BuyTransaction.cs
@@ -6,25 +6,39 @@ using System.Threading.Tasks;
 
 namespace KioskinHjaAstu
 {
-    class BuyTransaction : Transaction
+    public class BuyTransaction : Transaction
     {
-
         
-
-
-
-
-
-
+        Product boughtProduct;
+        decimal currentPrice;
+        
+        public BuyTransaction(User transactionUser, Product product)
+        {
+            transactionID = transactionCount++;
+            this.transactionUser = transactionUser;
+            boughtProduct = product;
+            amount = boughtProduct.price;
+        }
 
         public override void Execute()
         {
-            throw new NotImplementedException();
+            if (transactionUser.balance < amount || !boughtProduct.canBeBoughtOnCredit)
+            
+                throw new Exception("InsufficientCredits by user: " + transactionUser.userID +
+                                    "\n Can't afford product: " + boughtProduct.ToString());
+            
+            else
+                transactionUser.balance -= amount;
         }
 
         public override string ToString()
         {
-            throw new NotImplementedException();
+            return "Product being bought: " + boughtProduct.ToString() +
+                   "\nPrice: " + boughtProduct.price +
+                   "\nBuying user: " + transactionUser.userID.ToString() +
+                   "\nTime of transaction: " + timeOfTransaction.ToString() +
+                   "\nTransaction ID: " + transactionID.ToString();
+
         }
     }
 }
